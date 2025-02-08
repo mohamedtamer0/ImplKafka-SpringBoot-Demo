0a5816ca0c697077d20db8f7b075aab73ccb8e2d
mohamedtamer0 <mt4452242@gmail.com>
2025-02-08 19:10:07 +0200

    ImplKafka-SpringBoot-Demo

---
+++ src/main/java/com/tamer/ImplKafka_SpringBoot_Demo/ImplKafkaSpringBootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.tamer.ImplKafka_SpringBoot_Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImplKafkaSpringBootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImplKafkaSpringBootDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tamer/ImplKafka_SpringBoot_Demo/ImplKafkaSpringBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tamer.ImplKafka_SpringBoot_Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImplKafkaSpringBootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
