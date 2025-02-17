9049b8a2ec5629cab33fd4dfbd99374f2698aa8e
mohamedtamer0 <mt4452242@gmail.com>
2025-02-17 02:56:08 +0200

    update

---
+++ src/main/java/com/tamer/ImplKafka_SpringBoot_Demo/config/KafkaTopicConfig.java
@@ -0,0 +1,18 @@
+package com.tamer.ImplKafka_SpringBoot_Demo.config;
+
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopicConfig {
+
+    @Bean
+    public NewTopic kafkaLearnTopic(){
+        return TopicBuilder.name("kafkalearn")
+                .build();
+    }
+
+}
