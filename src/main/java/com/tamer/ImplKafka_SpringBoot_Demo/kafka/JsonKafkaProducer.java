7761e99119589bfeeeffe61b30977566f03ea933
mohamedtamer0 <mt4452242@gmail.com>
2025-02-17 04:10:02 +0200

    Update

--- src/main/java/com/tamer/ImplKafka_SpringBoot_Demo/kafka/JsonKafkaProducer.java
+++ src/main/java/com/tamer/ImplKafka_SpringBoot_Demo/kafka/JsonKafkaProducer.java
@@ -4,6 +4,7 @@ package com.tamer.ImplKafka_SpringBoot_Demo.kafka;
 import com.tamer.ImplKafka_SpringBoot_Demo.model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.Message;
@@ -12,6 +13,10 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class JsonKafkaProducer {
+
+    @Value("${spring.kafka.topic-json.name}")
+    private String topicJsonName;
+
 
     private static final Logger LOGGER = LoggerFactory.getLogger(JsonKafkaProducer.class);
 
@@ -26,7 +31,7 @@ public class JsonKafkaProducer {
         LOGGER.info(String.format("Message sent -> %s",data.toString()));
 
         Message<User> message = MessageBuilder.withPayload(data)
-                .setHeader(KafkaHeaders.TOPIC,"kafkalearn_json")
+                .setHeader(KafkaHeaders.TOPIC,topicJsonName)
                 .build();
 
         kafkaTemplate.send(message);
